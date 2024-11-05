4bcf6ccdf5500280accea83fce4d58848cf51f43
Rafael Santos <77756830+rafamsantos@users.noreply.github.com>
2024-11-05 15:13:27 +0000

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_lD39RfQvh', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6pe1tck837o0gt7kofh3p47p5h', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
